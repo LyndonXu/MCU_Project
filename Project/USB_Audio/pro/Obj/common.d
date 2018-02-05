672ec8005de8771064daee514b9f9b9839a25962
Lyndon <lyndonxu@163.com>
2018-02-05 19:42:33 +0800

    LyndonXu
    移除OBJ等临时文件

--- Project/USB_Audio/pro/Obj/common.d
+++
@@ -1,5 +0,0 @@
-.\obj\common.o: ..\src\common.c
-.\obj\common.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\obj\common.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\obj\common.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\obj\common.o: ..\inc\common.h
--- Project/USB_Audio/pro/Obj/ioctrl.d
+++
@@ -1,8 +0,0 @@
-.\obj\ioctrl.o: ..\src\IOCtrl.c
-.\obj\ioctrl.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\obj\ioctrl.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
-.\obj\ioctrl.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\obj\ioctrl.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
-.\obj\ioctrl.o: ..\inc\app_port.h
-.\obj\ioctrl.o: ..\inc\IOCtrl.h
-.\obj\ioctrl.o: D:\Program Files (x86)\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
